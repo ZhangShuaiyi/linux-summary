c5dcea91b7b2f76a3b234b24b5e012fb025155bf
ZhangShuaiyi <zhang_syi@163.com>
2016-11-28 17:50:42 +0800

    qemu command line use ubuntu vm

--- script/qemu-kvm/qemu-kvm-command/virtio_net/qemu_virtio_net_ubuntu.sh
+++ script/qemu-kvm/qemu-kvm-command/virtio_net/qemu_virtio_net_ubuntu.sh
@@ -5,12 +5,13 @@
         -s \
         -enable-kvm \
         -kernel /home/zsy/Data/nfs/ubuntu_linux/linux-4.4.0/arch/x86_64/boot/bzImage \
-        -hda /home/zsy/Workspace/code/linux-summary/data/linux-0.2.img \
+        -initrd /home/zsy/Data/qemu-kvm/initrd.img-4.4.0-38-generic \
+        -hda /home/imgs/ubuntu1604.qcow2 \
         -net nic,model=virtio \
         -net tap,ifname=tap0,script=no,downscript=no,vhost=on \
         -m 512 \
         -nographic \
-        -append "root=/dev/sda console=ttyS0" \
+        -append "root=/dev/mapper/ubuntu--vg-root console=ttyS0" \
         -serial pty
 # sudo ip link set tap0 master br0
 # sudo ip link set tap0 up
